f05fa86c3384d7283aa66e16e60a73edc74bb151
warrenbrown <warren@katapultmedia.com>
2018-05-02 19:23:29 -0500

    added angular to appp

--- mean_blog/app.js
+++ mean_blog/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const mongoose = require('moongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('./config/database');
+const path = require('path');
+
 const port = 3000;
 const app = express();
 
+mongoose.connect(config.uri, (err) => {
+  if (err) {
+    console.log('Unable to connect to database', err);
+  } else {
+    console.log('Connected to database: ' + config.db);
+  }
+});
+
+app.use(express.static(__dirname + '/client/dist/'));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+});
 app.listen(port);
 console.log('Server is running at http://localhost:' + port);
