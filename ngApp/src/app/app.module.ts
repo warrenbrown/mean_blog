088fedb776b99419a7629d9566ad7a97068e2141
warrenbrown <warren@katapultmedia.com>
2018-05-08 05:57:56 -0500

    initial commit

---
+++ ngApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { VideoCenterComponent } from './video-center/video-center.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    VideoCenterComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
